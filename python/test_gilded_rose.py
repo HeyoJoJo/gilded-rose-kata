5a959e90f913fad07cae409e80e23808791f7a35
jo <jotheprogrammer@gmail.com>
2019-08-05 15:26:01 -0500

    beginning refactor, adding addquality method

--- python/test_gilded_rose.py
+++ python/test_gilded_rose.py
@@ -8,7 +8,19 @@ class GildedRoseTest(unittest.TestCase):
         items = [Item("foo", 0, 0)]
         gilded_rose = GildedRose(items)
         gilded_rose.update_quality()
-        self.assertEquals("fixme", items[0].name)
+        self.assertEquals("foo", items[0].name)
+    
+    def test_add_quality_default(self):
+        items = [Item("foo", 0, 0)]
+        gilded_rose = GildedRose(items)
+        gilded_rose.add_quality(items[0])
+        self.assertEquals(1, items[0].quality)
+
+    def test_add_quality_by_non_default_amount(self):
+        items = [Item("foo", 0, 0)]
+        gilded_rose = GildedRose(items)
+        gilded_rose.add_quality(items[0], 5)
+        self.assertEquals(5, items[0].quality)
 
 if __name__ == '__main__':
     unittest.main()
